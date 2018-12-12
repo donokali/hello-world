9c7f2b6a395df78965cc5a2091248c6f4cea76ba
donokali <34419999+donokali@users.noreply.github.com>
2018-12-12 00:05:42 +0000

    Create Hello-world.java

---
+++ Hello-world.java
@@ -0,0 +1,9 @@
+//import input output libarary.
+//java is a object orionted language so the core of code must be in a class.
+// the method main to output the result of the programm.
+import java.util.io;
+public class HelloWorld{
+    public static void main(String[] args){
+      System.out.println("Hello World!");
+    }
+}
